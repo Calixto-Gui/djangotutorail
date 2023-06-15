a50de2c3d5d33cdddb318d6509c88f6f30240a75
Calixto-Gui <guilherme.calixto@gmail.com>
2023-06-15 20:08:38 -0300

    almost finish

---
+++ mysite/blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('post_title', 'post_text', 'pub_date', 'photo')
+        widgets = {
+            'pub_date': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment_text', 'like')
